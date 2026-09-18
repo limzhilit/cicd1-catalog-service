71d3cd26b081e5945292c5f8c7aae9eed0b576f7
limzhilit <g00440629@atu.ie>
2026-09-18 09:21:42 +0100

    initial Spring Boot catalog service

---
+++ src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/Cicd1CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1.catalog.cicd1catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1CatalogServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cicd1CatalogServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ie/atu/cicd1/catalog/cicd1catalogservice/Cicd1CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1.catalog.cicd1catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1CatalogServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
