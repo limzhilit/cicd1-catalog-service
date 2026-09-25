8aead338a43a859d7ea72d96814e80e66bbf4fac
limzhilit <g00440629@atu.ie>
2026-09-25 07:55:06 +0100

    apply format document

--- src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/model/Product.java
+++ src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/model/Product.java
@@ -1,6 +1,7 @@
 package ie.atu.cicd1.catalog.cicd1catalogservice.model;
 
 import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
 @Entity
@@ -11,16 +12,37 @@ public class Product {
   private Long id;
   private String name;
   private BigDecimal price;
-  public Product() {}
+
+  public Product() {
+  }
+
   public Product(Long id, String name, BigDecimal price) {
     this.id = id;
     this.name = name;
     this.price = price;
   }
-  public Long getId() { return id; }
-  public void setId(Long id) { this.id = id; }
-  public String getName() { return name; }
-  public void setName(String name) { this.name = name; }
-  public BigDecimal getPrice() { return price; }
-  public void setPrice(BigDecimal price) { this.price = price; }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
 }
--- src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/service/ProductService.java
+++ src/main/java/ie/atu/cicd1/catalog/cicd1catalogservice/service/ProductService.java
@@ -4,18 +4,20 @@ import ie.atu.cicd1.catalog.cicd1catalogservice.model.Product;
 import ie.atu.cicd1.catalog.cicd1catalogservice.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService {
   private final ProductRepository repository;
+
   public ProductService(ProductRepository repository) {
     this.repository = repository;
   }
+
   public List<Product> getAll() {
     return repository.findAll();
   }
+
   public Product create(Product product) {
     product.setId(null);
     return repository.save(product);
